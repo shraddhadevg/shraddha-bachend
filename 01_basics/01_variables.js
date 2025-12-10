9000d4017f9373ca0938865f2acb3547fd6b5c84
shraddhadevg <devgayyashraddha3105@gmail.com>
2025-12-10 12:46:36 +0000

    table

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountid = 144553
+let accountEmail = "shraddha@google. com"
+var accountPassword = "12345"
+accountcity = jaipur
+
+//accountid = 2// not allowed
+
+accountEmail = "shra@google.com"
+accountpassword = "21212121"
+accounrcity = "pune"
+
+console.log(accountid);
+console. table([accountid, accountEmail, accountPassword, accounrcity])
