1df24db9e0ce1a357d19ff6742d55e2385b6b31e
shraddhadevg <devgayyashraddha3105@gmail.com>
2025-12-14 10:33:06 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,47 @@
+"use stricts";  // treat all js code as newer version
+
+//alert( 3+3 ) // we are using node.js not browser
+
+//readability //
+console.log("shraddha");console.log("devgayya");
+
+// make it readable //
+
+console.log("shraddha");
+console.log("devgayya");
+
+console.log( 3
+     +
+      3 ); // code readability must be high
+
+      let name = "shraddha"
+      let age = 23
+      let isLoggedIn = false
+
+      /* datatypes*/
+
+      //number => 2 to power of 53
+      //string => "" or ''
+      // bigint 
+      // boolean => true/false
+      // null => stand alone value
+      // undefined =>
+      // // symbol => unique
+
+      //object
+
+
+      // datatypes//
+
+      console.log(typeof "shraddha"); // string
+      console.log(typeof age); // number
+      console.log(typeof null); // object
+      console.log(typeof undefined); // undefined
+      
+      
+      
+      
+      
+
+
+
