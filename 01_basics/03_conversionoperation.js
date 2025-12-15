e55fbfc7009d5cf7f81d9441dd800407bc3f4cfb
shraddhadevg <devgayyashraddha3105@gmail.com>
2025-12-15 13:20:25 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,44 @@
+let score = "shraddha"
+
+// console.log(typeof score);
+//  console.log(typeof (score));
+
+ let valueInNumber = Number (score)
+//  console.log(typeof (valueInNumber ))
+ //console.log(typeof (valueInNumber ));
+//console.log(valueInNumber);
+
+// NaN = not a number 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// undefined => NaN
+// Null => 0
+
+let isLoggedIn = "shraddha"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+
+// 1 => true
+// 0 => false
+// "" => true
+// "shraddha" => true
+
+// let someNumber = doSum(23,12)
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+
+
