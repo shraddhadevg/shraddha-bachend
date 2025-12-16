4b3efe805e3183e39d04aef22693f0d2e5b5817f
shraddhadevg <devgayyashraddha3105@gmail.com>
2025-12-16 12:44:10 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -35,10 +35,28 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-console.log(typeof stringNumber);
+//*****************************************************************operations**************************************************//
 
+let value = 3
+let negValue = -value
+// console.log(negValue);
 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*4);
+// console.log(2/6);
+// console.log(3%9);
+// console.log(4**7);
+
+let str1 = "hi"
+let str2 = "shraddha"
+let str3 = 3105
+
+let str4 = str1 + str2+ str3
+let shraddha  = console ;
+shraddha.log(str4);
 
 
