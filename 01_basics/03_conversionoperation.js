e309295efc15bc3d41924d9a9b3620efea93753f
shraddhadevg <devgayyashraddha3105@gmail.com>
2025-12-16 13:05:46 +0000

    operationsssss

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -55,8 +55,25 @@ let str1 = "hi"
 let str2 = "shraddha"
 let str3 = 3105
 
-let str4 = str1 + str2+ str3
-let shraddha  = console ;
-shraddha.log(str4);
+// let str4 = str1 + str2+ str3
+// let shraddha  = console ;
+// shraddha.log(str4);
+// console.log("1"+ 2);
+// console.log(1 +"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2
+let gameCounter = 100
+console.log(gameCounter);
+
+
+
+
 
 
