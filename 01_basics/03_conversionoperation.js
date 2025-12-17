a221eaf5769b2bd11188e4749c8c1f512207446d
shraddhadevg <devgayyashraddha3105@gmail.com>
2025-12-17 12:52:55 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -63,13 +63,15 @@ let str3 = 3105
 // console.log("1"+2+3);
 // console.log(1+2+"3");
 
-console.log(+true);
-console.log(+"");
+// console.log(+true);
+// console.log(+"");
 
 let num1, num2, num3 
 
 num1 = num2 = num3 = 2+2
+
 let gameCounter = 100
+gameCounter++;
 console.log(gameCounter);
 
 
