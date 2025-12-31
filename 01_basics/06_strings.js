b7abe4ecb54a62296199e92566662b32de89a20c
shraddhadevg <devgayyashraddha3105@gmail.com>
2025-12-31 11:51:29 +0000

    string2

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -3,6 +3,27 @@ const lastname = "devgayya"
 
 //console.log(name + lastname + "bharadi");
 
-console.log(`Hello my name is ${name} and my repo count is $`);
+//console.log(`Hello my name is ${name} and my repo count is $`);
 
+const gameName = new String('shraddh-aa')
+//console.log(gameName[7]);
+//console.log(gameName.__proto__);
+//console.log (gameName.length)
+//console.log (gameName. toLocaleUpperCase());
+//console.log (gameName. toLocaleLowerCase());
+//console.log (gameName. toLowerCase());
+//console.log (gameName. toWellFormed());
 
+//console.log(gameName.charAt(5));
+//console.log(gameName.indexOf("s"));
+//console.log(gameName.substring());
+
+const newString = gameName.substring(0, 6)
+console.log(newString );
+const anotherString = gameName.slice(-9,5)
+
+console.log(anotherString);  
+
+const newStringOne = "   shra   "
+console.log (newStringOne);
+console.log (newStringOne.trim());
