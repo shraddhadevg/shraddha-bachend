725519050a50eae2372a23274365c71889d65ec5
shraddhadevg <devgayyashraddha3105@gmail.com>
2026-01-01 11:25:22 +0000

    strings 3

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -5,7 +5,7 @@ const lastname = "devgayya"
 
 //console.log(`Hello my name is ${name} and my repo count is $`);
 
-const gameName = new String('shraddh-aa')
+const gameName = new String('shraddh-aa-com')
 //console.log(gameName[7]);
 //console.log(gameName.__proto__);
 //console.log (gameName.length)
@@ -27,3 +27,10 @@ console.log(anotherString);
 const newStringOne = "   shra   "
 console.log (newStringOne);
 console.log (newStringOne.trim());
+
+const url = "https://shraddha.com/shraddha%20devgayya"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes("shraddha"));
+
+console.log(gameName.split('-'));
