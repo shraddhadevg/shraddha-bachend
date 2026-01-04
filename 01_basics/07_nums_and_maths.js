21428c8f5023b13750649df1bc172202048b57ef
shraddhadevg <devgayyashraddha3105@gmail.com>
2026-01-04 10:53:38 +0000

    07-

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,24 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(5))
+
+const otherNumber = 1323.8966
+
+//console.log((otherNumber.toPrecision(4)));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//*******************************************maths***************************************************************************//
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.9));
+console.log(Math.floor(4.9));
